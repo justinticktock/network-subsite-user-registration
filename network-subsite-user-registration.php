121bb12a81199f49e63b5647a570232d3691ed22
Justin Fletcher <justinandco.com>
2017-03-28 22:41:18 +0100

    uprev'd the plugin version to force a flush of the rewrites.

--- network-subsite-user-registration.php
+++ network-subsite-user-registration.php
@@ -3,7 +3,7 @@
 Plugin Name: Network Subsite User Registration
 Plugin URI: http://justinandco.com/plugins/network-subsite-user-registration/
 Description: Allows subsite user registration for a Network (multisite) installation
-Version: 1.3.4.5
+Version: 1.3.4.6
 Author: Justin Fletcher
 Author URI: http://justinandco.com
 Text Domain: network-subsite-user-registration
